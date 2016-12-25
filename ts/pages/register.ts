e29e5cfc991cfae74d302fb4998ec4e0de847ca2
Zach Deibert <zachdeibert@gmail.com>
2016-12-25 16:55:38 +0000

    Fixed some things on the register page

--- ts/pages/register.ts
+++ ts/pages/register.ts
@@ -1,18 +1,30 @@
 /// <reference path="../page.ts" />
 /// <reference path="../navigation.ts" />
 /// <reference path="../apis.ts" />
+/// <reference path="../providers.ts" />
 
 namespace org.usd232.robotics.management.pages {
     import ApiController = org.usd232.robotics.management.apis.ApiController;
     import LoginRequest = org.usd232.robotics.management.apis.LoginRequest;
+    let Providers = (org.usd232.robotics.management as any).Providers; // VS doesn't like Liquid
 
     export class RegisterController extends AbstractPage {
         protected init(): void {
-            $(document).ready(function(){
-                $('select').material_select();
-            });
-            this.$scope.back = () => HistoryController.load("/");
+            this.$scope.back = () => HistoryController.back();
+        }
 
+        public constructor(name: string) {
+            super(name);
+            $(document).ready(() => {
+                let data: any = {};
+                let providers: string[] = Providers.getProviderNames();
+                for ( var i: number = 0; i < providers.length; ++i ) {
+                    data[providers[i]] = null;
+                }
+                ($(".autocomplete-register-phone-provider") as any).autocomplete({
+                    "data": data
+                });
+            });
         }
     }
 }
---
+++ ts/providers.ts
@@ -0,0 +1,587 @@
+namespace org.usd232.robotics.management {
+    export class Providers {
+        public static getProviderNames(): string[] {
+            return [
+                
+                    "3 River Wireless",
+                
+                    "Accessyou",
+                
+                    "ACS Wireless",
+                
+                    "Aio Wireless",
+                
+                    "AirCel",
+                
+                    "AirFire Mobile",
+                
+                    "Airtel",
+                
+                    "Airtel (India)",
+                
+                    "Airtel (India)",
+                
+                    "Alaska Communications",
+                
+                    "Aliant Canada",
+                
+                    "Allied Wireless",
+                
+                    "Alltel",
+                
+                    "Ameritech",
+                
+                    "Andhra Pradesh AirTel",
+                
+                    "Andhra Pradesh Idea",
+                
+                    "Assurance Wireless",
+                
+                    "AT&T",
+                
+                    "AT&T Enterprise Paging",
+                
+                    "AT&T Mobility",
+                
+                    "Beeline",
+                
+                    "Bell Canada",
+                
+                    "Bell Mobility",
+                
+                    "Bell Mobility (Canada)",
+                
+                    "BellSouth",
+                
+                    "Blue Sky Frog",
+                
+                    "Bluegrass Cellular",
+                
+                    "Bluesky Communications",
+                
+                    "Boost Mobile",
+                
+                    "Bouygues Telecom",
+                
+                    "Box Internet Services",
+                
+                    "BPL Mobile",
+                
+                    "C Spire Wireless",
+                
+                    "Carolina West Wireless",
+                
+                    "CellCom",
+                
+                    "Cellular One",
+                
+                    "Cellular South",
+                
+                    "Centennial Wireless",
+                
+                    "CenturyTel",
+                
+                    "Chariton Valley Wireless",
+                
+                    "Chat Mobility",
+                
+                    "Chennai Airtel",
+                
+                    "Chennai RPG Cellular",
+                
+                    "Chennai Skycell",
+                
+                    "China Mobile",
+                
+                    "Cincinnati Bell",
+                
+                    "Cingular",
+                
+                    "Claro (Argentina)",
+                
+                    "Claro (Brazil)",
+                
+                    "Claro (Colombia)",
+                
+                    "Claro (Nicaragua)",
+                
+                    "Claro (Puerto Rico)",
+                
+                    "Clearnet",
+                
+                    "Cleartalk",
+                
+                    "Comcast",
+                
+                    "Comviq",
+                
+                    "Corr Wireless Communications",
+                
+                    "Cricket Wireless",
+                
+                    "CSL",
+                
+                    "CTI Móvil",
+                
+                    "Delhi Airtel",
+                
+                    "Delhi Hutch",
+                
+                    "Digicel",
+                
+                    "Dobson",
+                
+                    "DT T-Mobile",
+                
+                    "DTC",
+                
+                    "Dutchtone",
+                
+                    "E-Plus",
+                
+                    "Edge Wireless",
+                
+                    "Element Mobile",
+                
+                    "EMT",
+                
+                    "Emtel",
+                
+                    "Escotel",
+                
+                    "Esendex",
+                
+                    "Esendex (Spain)",
+                
+                    "Fido",
+                
+                    "Firmensms",
+                
+                    "Freebie SMS",
+                
+                    "General Communications Inc.",
+                
+                    "German T-Mobile",
+                
+                    "Globalstar",
+                
+                    "Globul",
+                
+                    "Goa Airtel",
+                
+                    "Goa BPLMobil",
+                
+                    "Goa Idea Cellular",
+                
+                    "Golden State Cellular",
+                
+                    "Golden Telecom",
+                
+                    "Greatcall",
+                
+                    "Gujarat Airtel",
+                
+                    "Gujarat Celforce",
+                
+                    "Gujarat Fascel",
+                
+                    "Gujarat Idea Cellular",
+                
+                    "Guyana T&T",
+                
+                    "Haryana Airtel",
+                
+                    "Haryana Escotel",
+                
+                    "Hawaiian Telecom",
+                
+                    "Helio",
+                
+                    "Himachai Pradesh Airtel",
+                
+                    "Houston Cellular",
+                
+                    "HSL Mobile",
+                
+                    "I-Wireless (Sprint PCS)",
+                
+                    "I-Wireless (T-Mobile)",
+                
+                    "ICE",
+                
+                    "Idea Cellular",
+                
+                    "Illinois Valley Cellular",
+                
+                    "Inland Cellular Telephone",
+                
+                    "JSM Tele-Page",
+                
+                    "Kajeet",
+                
+                    "Karnataka Airtel",
+                
+                    "Kerala Airtel",
+                
+                    "Kerala Escotel",
+                
+                    "Kolkata Airltel",
+                
+                    "Koodo Mobile",
+                
+                    "Kyivstar",
+                
+                    "Lauttamus Communication",
+                
+                    "LMT",
+                
+                    "LongLines",
+                
+                    "Lynx Mobility",
+                
+                    "M1",
+                
+                    "Madhya Pradesh Airtel",
+                
+                    "Maharashtra Airtel",
+                
+                    "Maharashtra BPL Mobile",
+                
+                    "Maharashtra Idea",
+                
+                    "Maharashtra Idea Cellular",
+                
+                    "Manitoba Telecom Systems",
+                
+                    "Más Móvil",
+                
+                    "MCI",
+                
+                    "Mediaburst",
+                
+                    "Meteor",
+                
+                    "Metro PCS",
+                
+                    "Metrocall",
+                
+                    "Metrocall 2-way",
+                
+                    "Microcell",
+                
+                    "Midwest Wireless",
+                
+                    "MiWorld",
+                
+                    "Mobilcomm",
+                
+                    "Mobileone",
+                
+                    "Mobilfone",
+                
+                    "Mobility Bermuda",
+                
+                    "Mobiltel",
+                
+                    "Mobistar Belgium",
+                
+                    "Mobitel (Sri Lanka)",
+                
+                    "Mobitel (Tanzania)",
+                
+                    "Mobitel (Srbija)",
+                
+                    "Movistar",
+                
+                    "Movistar (Argentina)",
+                
+                    "Movistar (Colombia)",
+                
+                    "Movistar (Latin America)",
+                
+                    "Movistar (Spain)",
+                
+                    "Movistar (Uruguay)",
+                
+                    "MTN",
+                
+                    "MTS Mobility",
+                
+                    "Mumbai Airtel",
+                
+                    "Mumbai BPL Mobile",
+                
+                    "My-Cool-SMS",
+                
+                    "Netcom",
+                
+                    "Nextech",
+                
+                    "Nextel",
+                
+                    "Nextel (Argentina)",
+                
+                    "Nextel (Mexico)",
+                
+                    "Ntelos",
+                
+                    "O2",
+                
+                    "OgVodafone",
+                
+                    "Ola",
+                
+                    "One Connect Austria",
+                
+                    "OnlineBeep",
+                
+                    "Optus Mobile",
+                
+                    "Orange",
+                
+                    "Orange (Netherlands)",
+                
+                    "Orange (UK)",
+                
+                    "Orange (Mumbai)",
+                
+                    "Oskar",
+                
+                    "P&T Luxembourg",
+                
+                    "Page Plus Cellular",
+                
+                    "PC Telecom",
+                
+                    "PCS One",
+                
+                    "Personal",
+                
+                    "Personal Communication",
+                
+                    "Pioneer Cellular",
+                
+                    "Plus",
+                
+                    "Pocket Wireless",
+                
+                    "Polkomtel",
+                
+                    "Pondicherry BPL Mobile",
+                
+                    "President’s Choice",
+                
+                    "Primtel",
+                
+                    "Project Fi",
+                
+                    "Public Service Cellular",
+                
+                    "Qwest Wireless",
+                
+                    "Red Pocket Mobile",
+                
+                    "Republic Wireless",
+                
+                    "Rogers AT&T Wireless",
+                
+                    "Rogers Wireless",
+                
+                    "Safaricom",
+                
+                    "SaskTel",
+                
+                    "Satelindo GSM",
+                
+                    "Satellink",
+                
+                    "SCS-900",
+                
+                    "Sendega",
+                
+                    "Setar Mobile email (Aruba)",
+                
+                    "SFR France",
+                
+                    "Síminn",
+                
+                    "Simple Freedom",
+                
+                    "Simple Mobile",
+                
+                    "Smart Telecom",
+                
+                    "SMS Broadcast",
+                
+                    "SMS Central",
+                
+                    "SMSPUP",
+                
+                    "Solo Mobile",
+                
+                    "South Central Communications",
+                
+                    "Southern LINC",
+                
+                    "Southwestern Bell",
+                
+                    "Spikko",
+                
+                    "Sprint",
+                
+                    "Straight Talk (AT&T)",
+                
+                    "Straight Talk (Other)",
+                
+                    "Straight Talk (Sprint)",
+                
+                    "Straight Talk (T-Mobile)",
+                
+                    "Straight Talk (Verizon)",
+                
+                    "Sumcom",
+                
+                    "Sunrise Communications",
+                
+                    "Sunrise Mobile",
+                
+                    "Surewest Communications",
+                
+                    "Swisscom",
+                
+                    "Syringa Wireless",
+                
+                    "T-Mobile (Australia)",
+                
+                    "T-Mobile (Austria)",
+                
+                    "T-Mobile (Croatia)",
+                
+                    "T-Mobile (Germany)",
+                
+                    "T-Mobile (Netherlands)",
+                
+                    "T-Mobile (USA)",
+                
+                    "T-Mobile (UK)",
+                
+                    "Tamil Nadu Aircel",
+                
+                    "Tamil Nadu Airtel",
+                
+                    "Tamil Nadu BPL Mobile",
+                
+                    "Telcel",
+                
+                    "Tele2",
+                
+                    "Tele2 Latvia",
+                
+                    "Telecom",
+                
+                    "Teleflip",
+                
+                    "Telefonica Movistar",
+                
+                    "Telenor",
+                
+                    "TeletopiaSMS",
+                
+                    "Teletouch",
+                
+                    "Telia Denmark",
+                
+                    "TellusTalk",
+                
+                    "Telstra",
+                
+                    "Telus",
+                
+                    "Telus Mobility",
+                
+                    "Tigo",
+                
+                    "TIM",
+                
+                    "Ting",
+                
+                    "Tracfone",
+                
+                    "TracFone (Prepaid)",
+                
+                    "Triton",
+                
+                    "TSR Wireless",
+                
+                    "Txtlocal",
+                
+                    "U.S. Cellular",
+                
+                    "UMC",
+                
+                    "Unicel",
+                
+                    "UniMóvil Corporation",
+                
+                    "Union Wireless",
+                
+                    "Uraltel",
+                
+                    "US Cellular",
+                
+                    "US West",
+                
+                    "USA Mobility",
+                
+                    "UTBox",
+                
+                    "Uttar Pradesh Escotel",
+                
+                    "Uttar Pradesh West Escotel",
+                
+                    "Verizon Wireless",
+                
+                    "Vessotel",
+                
+                    "Viaero",
+                
+                    "Virgin Mobile",
+                
+                    "Virgin Mobile (Canada)",
+                
+                    "Virgin Mobile (UK)",
+                
+                    "Virgin Mobile (USA)",
+                
+                    "Vivo",
+                
+                    "Vodacom",
+                
+                    "Vodafone (Germany)",
+                
+                    "Vodafone (Italy)",
+                
+                    "Vodafone (New Zealand)",
+                
+                    "Vodafone (Spain)",
+                
+                    "Vodafone (Japan)",
+                
+                    "Vodafone (UK)",
+                
+                    "Voyager Mobile",
+                
+                    "West Central Wireless",
+                
+                    "Western Wireless",
+                
+                    "Wind Mobile",
+                
+                    "Wyndtell",
+                
+                    "XIT Communications",
+                
+            ];
+        }
+    }
+}
